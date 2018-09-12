fe45b6b8a85108b4f771cee2cad743d48d6b4dd7
Xiangzhi Liang <xiang_zhi_@126.com>
2018-09-12 16:05:01 +0800

    Create select.genome.pl

---
+++ select.genome.pl
@@ -0,0 +1,36 @@
+my $in = shift;
+my %hash;
+open IN,$in||die;
+while(<IN>){
+    chomp;
+    next if(/^#/);
+    my @arr = split(/\t/);
+    $hash{$arr[6]}{$arr[4]}=$_;
+    $hash{$arr[6]}{$arr[11]}=$_;
+}
+
+foreach my $key(keys %hash){
+
+        if(defined $hash{$key}{"reference genome"}){
+             print $hash{$key}{"reference genome"}."\n";
+          
+        }elsif(defined $hash{$key}{"representative genome"}){
+             print $hash{$key}{"representative genome"}."\n";
+              
+        }elsif(defined $hash{$key}{"Complete Genome"}){
+             print $hash{$key}{"Complete Genome"}."\n";
+             
+        }elsif(defined $hash{$key}{"Chromosome"}){
+             print $hash{$key}{"Chromosome"}."\n";
+             
+        }elsif(defined $hash{$key}{"Scaffold"}){
+             print $hash{$key}{"Scaffold"}."\n";
+             
+        }else{
+             print $hash{$key}{"Contig"}."\n";
+            
+        }
+    
+}
+
+close IN;
